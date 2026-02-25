1b5d8c8bb6649466714a6e8dde5a734457342b48
Ирина Новикова <irishnovikova@icloud.com>
2026-02-26 01:39:15 +0700

    Initial commit

---
+++ ITindr/ITindr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITindr
+//
+//  Created by Ирина Новикова on 26.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
